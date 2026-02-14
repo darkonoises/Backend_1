b2f9c97f5da60418364c1f957ad6bf08affbe936
kevin florec <darkonoises@gmail.com>
2026-02-14 12:05:20 -0500

    3ra version parcial

--- src/backend_1/Backend_1.java
+++ src/backend_1/Backend_1.java
@@ -1,28 +1,32 @@
 
 package backend_1;
 
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 
 public class Backend_1 {
 
-  
+  final static int Current_Year = 2026;
+    
     public static void main(String[] args) {
         
-           
-    Scanner entrada = new Scanner(System.in).useLocale(Locale.US);
-    
-        System.out.println("Hola Estudiante");
-        System.out.println("Dime cual es tu nota: ");
+        final String GREETING_MESSAGE ="Saludo estudiantes de java";
+        final int Current_Year = 2026;
         
-        float number = entrada.nextFloat();
-        
-        if (number >= 3.0){System.out.println("Felicidades, Aprobaste");}
-        else {System.out.println("Lo siento, no aprobaste. Sigue intentandolo");}
-    
-    entrada.close();
-    
+        String [] nombres = {"Juan","Maria","Pedro","Ana","Luis"};
+                int [] nacimiento = {2000, 1998, 2002, 1995, 2001};
+                
+                List<int[]> edades = new ArrayList<>();        
+                        
+    }
+                static void showMessage(String GREETING_MESSAGE){
+                    System.out.println(GREETING_MESSAGE);}
+                
+                static int calcularEdad (int birthyear){
+                return Current_Year - birthyear;
+                }
+                
     
     
     
